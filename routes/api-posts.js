24b67595f4e7c0416db4e9c5bae30a73c3221ca7
Lachlan Heywood <lachlan@heywood.net.au>
2020-11-06 16:52:02 -0500

    update api with Posts

---
+++ routes/api-posts.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
+
+// Get All Posts
+// GET /api/v1/posts/
+router.get('/', function(req, res) {
+  models.Post.findAll()
+    .then(posts => {
+      res.json(posts)
+    })
+});
+
+// Get 1 Post by ID
+// GET /api/v1/posts/102
+router.get('/:id', (req, res) => {
+  models.Post.findByPk(req.params.id)
+    .then(post => {
+      if (post) {
+        res.json(post)
+      } else {
+        res.status(404).json({
+          error: 'Post not found'
+        })
+      }
+    })
+})
+
+// Update Post
+// PUT /api/v1/posts/101
+router.put('/:id', (req, res) => {
+  if (!req.body || !req.body.author || !req.body.title || !req.body.content || !req.body.published ) {
+    res.status(400).json({
+      error: 'Please submit all required fields'
+    })
+    return;
+  }
+  models.Post.update({
+    author: req.body.author,
+    title: req.body.title,
+    content: req.body.content,
+    published: req.body.published
+  }, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(updated => {
+      if (updated && updated[0] === 1) {
+        res.status(202).json({
+          success: 'Post Updated'
+        });
+      } else {
+        res.status(404).json({
+          error: 'Post not found'
+        })
+      }
+    })
+})
+
+// Create new Post
+// POST /api/v1/posts/
+router.post('/', (req, res) => {
+  if (!req.body || !req.body.author || !req.body.title || !req.body.content || !req.body.published ) {
+    res.status(400).json({
+      error: 'Please submit all required fields'
+    })
+    return;
+  }
+  models.Post.create({
+    author: req.body.author,
+    title: req.body.title,
+    content: req.body.content,
+    published: req.body.published
+  })
+    .then(post => {
+      res.status(201).json(post)
+    })
+})
+
+// Delete Post
+// DELETE /api/v1/posts/101
+router.delete('/:id', (req, res) => {
+  models.Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(deleted => {
+      if (deleted === 1) {
+        res.status(202).json({
+          success: 'Post deleted'
+        });
+      } else {
+        res.status(404).json({
+          error: 'Post not found'
+        })
+      }
+    })
+})
+
+module.exports = router;
